278bf0fee2dbfbc3e3e65086a8250f37987c05f0
JonathanArns <jonathan.arns@googlemail.com>
2024-02-28 15:59:59 +0100

    define a basic storage abstraction

---
+++ src/storage/counters.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+use super::Operation;
+
+/// Prelim def of a key
+pub type Key = u64;
+/// Prelim def of a stored Value
+pub type Value = i64;
+
+#[derive(Debug)]
+pub enum CounterOp {
+    /// weak
+    Read{key: Key},
+    /// weak
+    Add{key: Key, val: Value},
+    /// weak
+    Subtract{key: Key, val: Value},
+    /// strong
+    Set{key: Key, val: Value},
+}
+
+impl Operation for CounterOp {
+    type State = HashMap<Key, Value>;
+    type ReadVal = Option<Value>;
+
+    fn is_weak(&self) -> bool {
+        match *self {
+            Self::Read{..} => true,
+            Self::Add{..} => true,
+            Self::Subtract{..} => true,
+            Self::Set{..} => false,
+        }
+    }
+
+    fn apply(&self, state: &mut Self::State) -> Option<Self::ReadVal> {
+        match *self {
+            Self::Read { key } => {
+                Some(state.get(&key).map(|v| v.to_owned()))
+            },
+            Self::Add { key, val } => {
+                if let Some(v) = state.get_mut(&key) {
+                    *v += val;
+                } else {
+                    state.insert(key, val);
+                }
+                None
+            },
+            Self::Subtract { key, val } => {
+                if let Some(v) = state.get_mut(&key) {
+                    *v -= val;
+                } else {
+                    state.insert(key, -val);
+                }
+                None
+            },
+            Self::Set { key, val } => {
+                state.insert(key, val);
+                None
+            },
+        }
+    }
+}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,36 +1,63 @@
-/// Prelim def of a key
-pub type Key = u64;
-/// Prelim def of a stored Value
-pub type Value = u64;
+use std::collections::HashMap;
+
+use crate::network::NodeId;
+
+pub mod counters;
 
 /// A snapshot identifier that enables processes to construct the snapshot
 pub struct Snapshot {
-    vector_clock: Vec<u64>,
+    vec: Vec<u64>,
 }
 
-#[derive(Debug)]
-pub enum DemonOp {
-    /// weak
-    Add{key: Key, val: Value},
-    /// strong
-    Set{key: Key, val: Value},
-}
-
-impl DemonOp {
-    fn is_weak(&self) -> bool {
-        match *self {
-            Self::Add{..} => true,
-            Self::Set{..} => false,
+impl Snapshot {
+    pub fn merge(&self, other: &Self) -> Self {
+        let mut vector = vec![];
+        for i in 0..self.vec.len() {
+            vector.push(self.vec[i].max(other.vec[i]));
         }
+        Self { vec: vector }
     }
 }
 
-pub struct Transaction {
-    ops: Vec<DemonOp>,
+pub trait Operation {
+    type State: Default;
+    type ReadVal;
+
+    fn is_weak(&self) -> bool;
+    fn apply(&self, state: &mut Self::State) -> Option<Self::ReadVal>;
 }
 
-pub trait DemonStorage {
-    fn weak_op(&mut self, op: DemonOp) -> Option<Value>;
+pub struct Transaction<O: Operation> {
+    ops: Vec<O>,
+    snapshot: Snapshot,
+}
 
-    fn transaction(&mut self, t: Transaction, snapshot: Snapshot) -> Option<Value>;
+pub struct Storage<O: Operation>{
+    weak_logs: HashMap<NodeId, Vec<O>>,
+    transactions: Vec<Transaction<O>>,
+}
+
+impl<O: Operation> Storage<O> {
+    pub fn new(nodes: Vec<NodeId>) -> Self {
+        let mut weak_logs = HashMap::new();
+        for id in nodes {
+            weak_logs.insert(id, vec![]);
+        }
+        Self {
+            weak_logs,
+            transactions: vec![],
+        }
+    }
+
+    pub fn current_snapshot(&self) -> Snapshot {
+        todo!()
+    }
+
+    pub fn exec_weak(&mut self, op: O) -> Option<O::ReadVal> {
+        todo!()
+    }
+
+    pub fn exec_transaction(&mut self, t: Transaction<O>) -> Vec<O::ReadVal> {
+        todo!()
+    }
 }
