54c128ffec7467415cb576d46b50389c8eaf55a1
JonathanArns <jonathan.arns@googlemail.com>
2024-03-13 15:06:20 +0100

    better query notation and add basic automated tests

--- repl.py
+++ repl.py
@@ -2,6 +2,7 @@
 
 import requests
 import sys
+import time
 
 query_mode = "weak"
 
@@ -25,16 +26,16 @@ if addr == None:
 else:
     print(f"found demon at `{addr}`\nswitch to different node with `addr <address>`")
 
-
-while True:
-    line = str(input("> ")).strip()
+def process_input(line):
+    global addr
+    global query_mode
     parts = line.split(" ", 1)
     command = parts[0]
     args = parts[1] if len(parts) > 1 else ""
     query = command
 
     if "exit" == command:
-        break
+        exit("Goodbye")
 
     elif "addr" == command:
         try:
@@ -46,25 +47,48 @@ while True:
                 print(f"wrong response: {resp.text}")
         except:
             print("could not connect")
-        continue
+        return
 
     elif "weak" == command or "w" == command:
         query_mode = "weak"
         if args:
             query = args
         else:
-            continue
+            return
 
     elif "strong" == command or "s" == command:
         query_mode = "strong"
         if args:
             query = args
         else:
-            continue
+            return
+
+    elif "test" == command:
+        run_tests()
+        return
 
     if not addr:
         print(f"not connected. connect with `addr <address>`")
     resp = requests.post(f"http://{addr}/{query_mode}", data=query)
     print(resp.text)
 
-print("Goodbye.")
+def run_tests():
+    run_test("fabi's deadlock", ["1+1", "w r1", "1+12", "r1", "s r1", "r1", "1+2", "1-12", "r1", "1=4", "r1", "w 2+1", "r2", "2=4", "s 2=7"])
+    run_test("fabi's deadlock 2", ["w 1=1", "s 1=2", "1+2", "r2", "r1", "w 1=1", "r1", "s 1=2"])
+
+def run_test(name, scenario):
+    global addr
+    print(f"Running test: {name}")
+    if not addr:
+        print(f"not connected. connect with `addr <address>`")
+        return
+    for cmd in scenario:
+        time.sleep(0.1)
+        print(f"> {cmd}")
+        process_input(cmd)
+    print(f"Completed test: {name}")
+
+
+while True:
+    line = str(input("> ")).strip()
+    process_input(line)
--- src/api/query_parser.rs
+++ src/api/query_parser.rs
@@ -11,30 +11,26 @@ pub fn parse_counter_query(text: &str) -> anyhow::Result<Query<CounterOp>> {
 }
 
 pub fn parse_counter_op(text: &str) -> anyhow::Result<CounterOp> {
-    let (op, operands) = text.split_at(1);
-    match op {
-        "p" => {
-            let (key, val) = operands.split_once(":").ok_or_else(|| anyhow!("only one operand"))?;
-            let key = key.parse::<counters::Key>()?;
-            let val = val.parse::<counters::Value>()?;
-            Ok(CounterOp::Add{key, val})
-        },
-        "m" => {
-            let (key, val) = operands.split_once(":").ok_or_else(|| anyhow!("only one operand"))?;
-            let key = key.parse::<counters::Key>()?;
-            let val = val.parse::<counters::Value>()?;
-            Ok(CounterOp::Subtract{key, val})
-        },
-        "s" => {
-            let (key, val) = operands.split_once(":").ok_or_else(|| anyhow!("only one operand"))?;
-            let key = key.parse::<counters::Key>()?;
-            let val = val.parse::<counters::Value>()?;
-            Ok(CounterOp::Set{key, val})
-        },
-        "r" => {
-            let key = operands.parse::<counters::Key>()?;
-            Ok(CounterOp::Read{key})
-        },
-        _ => Err(anyhow!("bad query"))
+    if let Some((key, val)) = text.split_once("+") {
+        let key = key.parse::<counters::Key>()?;
+        let val = val.parse::<counters::Value>()?;
+        Ok(CounterOp::Add{key, val})
+    } else if let Some((key, val)) = text.split_once("-") {
+        let key = key.parse::<counters::Key>()?;
+        let val = val.parse::<counters::Value>()?;
+        Ok(CounterOp::Subtract{key, val})
+    } else if let Some((key, val)) = text.split_once("=") {
+        let key = key.parse::<counters::Key>()?;
+        let val = val.parse::<counters::Value>()?;
+        Ok(CounterOp::Set{key, val})
+    } else {
+        let (op, operands) = text.split_at(1);
+        match op {
+            "r" => {
+                let key = operands.parse::<counters::Key>()?;
+                Ok(CounterOp::Read{key})
+            },
+            _ => Err(anyhow!("bad query"))
+        }
     }
 }
