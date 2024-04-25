219dbdfd87a09a125b5b22e270e294b4961cf86b
JonathanArns <jonathan.arns@googlemail.com>
2024-04-25 18:32:10 +0200

    improve microbenchmark

--- microbench/src/main.rs
+++ microbench/src/main.rs
@@ -1,20 +1,22 @@
 use std::{env, time::Duration};
 
 use lazy_static::lazy_static;
-use tokio::{self, time::Instant};
+use serde::Deserialize;
+use tokio::{self, sync::watch};
 use reqwest;
 use rand::{Rng, thread_rng};
 
 // benchmark settings
 const STRONG_RATIO: f64 = 0.3;
 const REQUEST_TIMEOUT: Duration = Duration::from_secs(3);
-const NUM_CLIENTS: usize = 10;
-const NUM_REQUESTS: usize = 10;
+const NUM_CLIENTS: usize = 200;
+const DURATION: Duration = Duration::from_secs(3);
 
 lazy_static!{
     static ref TARGET_DOMAINS: Vec<String> = env::args().skip(1).collect();
 }
 
+#[derive(Clone, Debug, Deserialize)]
 struct Measurement {
     latency: Duration,
 }
@@ -24,35 +26,62 @@ async fn main() {
     if TARGET_DOMAINS.len() < 1 {
         panic!("missing arguments: target domain(s)");
     }
+
+    let (sender, watcher) = watch::channel(false);
+
+    // set up the clients
+    let mut measurements = vec![];
     let mut futures = vec![];
     for i in 0..NUM_CLIENTS {
-        futures.push(tokio::spawn(run_client(&TARGET_DOMAINS[i % TARGET_DOMAINS.len()])));
+        let watcher_handle = watcher.clone();
+        futures.push(tokio::spawn(run_client(watcher_handle, &TARGET_DOMAINS[i % TARGET_DOMAINS.len()])));
     }
+
+    // Run the clients for the set duration
+    sender.send(true).unwrap();
+    tokio::time::sleep(DURATION).await;
+    sender.send(false).unwrap();
+
+    // collect the results
     for f in futures {
-        f.await.unwrap();
+        measurements.extend_from_slice(&f.await.unwrap());
     }
+
+    println!(
+        "mean latency: {}ms, throughput: {} operations per second",
+        measurements.iter().map(|m| m.latency.as_millis() as f64).sum::<f64>() / measurements.len() as f64,
+        measurements.len() / DURATION.as_secs() as usize,
+    );
 }
 
-async fn run_client(domain: &str) {
+async fn run_client(mut watcher: watch::Receiver<bool>, domain: &str) -> Vec<Measurement> {
     let client = reqwest::Client::new();
     let mut measurements = vec![];
-    for _ in 0..NUM_REQUESTS {
+    
+    // wait for the benchmark to start
+    watcher.wait_for(|v| *v).await.unwrap();
+    watcher.mark_unchanged();
+    loop {
         let query = generate_query();
-        let start_time = Instant::now();
         let response = client.post(format!("{}/query", domain))
             .body(query.clone())
             .timeout(REQUEST_TIMEOUT)
             .send()
             .await;
-        if response.is_err() {
+        let measurement: Measurement = if let Ok(r) = response {
+            serde_json::from_str(&r.text().await.unwrap()).unwrap()
+        } else {
             println!("timed out on operation: {:?}", query);
-            return
+            return vec![]
+        };
+        measurements.push(measurement);
+
+        if watcher.has_changed().unwrap() {
+            break
         }
-        let latency = start_time.elapsed();
-        measurements.push(Measurement{ latency });
     }
     
-    println!("mean latency: {}ms", measurements.iter().map(|m| m.latency.as_millis() as f64).sum::<f64>() / measurements.len() as f64);
+    measurements
 }
 
 fn generate_query() -> String {
