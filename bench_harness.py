270e4ae91779d29c0bcc683c6223ac9bde39d4fe
JonathanArns <jonathan.arns@googlemail.com>
2024-05-16 16:14:05 +0200

    add generators to bench harness to parse and expand settings

--- bench_harness.py
+++ bench_harness.py
@@ -3,38 +3,133 @@
 import requests
 import sys
 import time
+import copy
+import json
 
+nodes = {
+    "bob": {
+        "ip": "localhost",
+        "control_port": 5000,
+        "db_port": 8080,
+        "internal_addr": "bob:1234"
+    },
+    "alice": {
+        "ip": "localhost",
+        "control_port": 5001,
+        "db_port": 8081,
+        "internal_addr": "alice:1234"
+    },
+    "fabi": {
+        "ip": "localhost",
+        "control_port": 5002,
+        "db_port": 8082,
+        "internal_addr": "fabi:1234"
+    },
+}
 
 cluster_config = {
     "proto": "demon",
     "datatype": "counter",
-    "nodes": [
-        {
-            "ip": "localhost",
-            "control_port": 5000,
-            "db_port": 8080,
-            "internal_addr": "bob:1234"
-        },
-        {
-            "ip": "localhost",
-            "control_port": 5001,
-            "db_port": 8081,
-            "internal_addr": "alice:1234"
-        },
-        {
-            "ip": "localhost",
-            "control_port": 5002,
-            "db_port": 8082,
-            "internal_addr": "fabi:1234"
-        },
-    ]
+    "nodes_ids": ["bob", "alice", "fabi"]
 }
+
+bench_config = {
+    "cluster_config": cluster_config,
+    "type": "micro", # tpcc, micro
+    "settings": {
+        "strong_ratio": 0.2,
+        "read_ratio": 0.0,
+        "num_clients": 100,
+        "key_range": 10,
+        "duration": 10,
+    }
+}
+
+
+def run_benches_from_file(path):
+    global nodes
+    data = json.load(path)
+    nodes = data["nodes"]
+    for conf in data["multi_bench_configs"]:
+        for bench_config in expand_multi_bench_config(conf):
+            run_bench(bench_config)
+
+def expand_multi_bench_config(multi_config):
+    for proto in multi_confg["cluster_config"]["proto"]:
+        for datatype in multi_confg["cluster_config"]["datatype"]:
+            cluster_config = {
+                "proto": proto,
+                "datatype": datatype,
+                "node_ids": multi_confg["cluster_config"]["node_ids"]
+            }
+            
+            if "micro" == multi_config["type"]:
+                for settings in expand_micro_bench_settings(multi_config["settings"]):
+                    yield {
+                        "cluster_config": cluster_config,
+                        "type": multi_config["type"],
+                        "settings": settings,
+                    }
+            elif "tpcc" == multi_config["type"]:
+                print("missing impl for tpcc in bench harness")
+
+def expand_micro_bench_settings(settings):
+    for strong_ratio in settings["strong_ratio"]:
+        for read_ratio in settings["read_ratio"]:
+            for num_clients in settings["num_clients"]:
+                for key_range in settings["key_range"]:
+                    for duration in settings["duration"]:
+                        yield {
+                            "strong_ratio": strong_ratio
+                            "read_ratio": read_ratio,
+                            "num_clients": num_clients,
+                            "key_range": key_range,
+                            "duration": duration,
+                        }
+
+def run_bench(bench_config):
+    """
+    Runs a benchmark according to the specified config.
+
+    TODO: collect results
+    TODO: tpcc
+    """
+    reconfigured = ensure_cluster_state(bench_config["cluster_config"])
+    if "micro" == bench_config["type"]:
+        resp = requests.post(f"http://{addr}/bench", json=bench_config["settings"])
+        print(resp.json())
+    elif "tpcc" == bench_config["type"]:
+        print("tpcc bench not implemented in harness yet")
+    else:
+        print("Bad benchmark type. choose one of: micro, tpcc")
+
+current_cluster_config = None
+def ensure_cluster_state(cluster_config):
+    """
+    Makes sure the cluster is configured according to the arguments.
+    Returns `True` if the cluster was reconfigured in the process.
+    """
+    global current_cluster_config
+    old = current_cluster_config
+    new = cluster_config
+    if old is None \
+        or old["proto"] != new["proto"] \
+        or old["datatype"] != new["datatype"] \
+        or set(old["node_ids"]) == set(new["node_ids"]):
+        stop_servers(old)
+        start_servers(new)
+        current_cluster_config = copy.deepcopy(new)
+        return True
+    else:
+        return False
 
 def stop_servers(cluster_config):
     """
     Blocks until all servers that are part of the cluster config are stopped.
     """
-    for node in cluster_config["nodes"]:
+    global nodes
+    for node_id in cluster_config["node_ids"]:
+        node = nodes[node_id]
         requests.post(f"http://{node['ip']}:{node['control_port']}/stop")
 
 def start_servers(cluster_config):
@@ -42,8 +137,10 @@ def start_servers(cluster_config):
     Starts a cluster with the given config.
     Only returns once all replicas pass the health check.
     """
+    global nodes
     root_node = None
-    for node in cluster_config["nodes"]:
+    for node_id in cluster_config["node_ids"]:
+        node = nodes[node_id]
         config = {
             "proto": cluster_config["proto"],
             "datatype": cluster_config["datatype"],
@@ -67,7 +164,5 @@ def start_servers(cluster_config):
         except:
             attempts += 1
             time.sleep(attempts)
-
-
-stop_servers(cluster_config)
-start_servers(cluster_config)
+    # just to make sure everything had more than enough time to be fully running
+    time.sleep(1)
--- server_controller.py
+++ server_controller.py
@@ -48,4 +48,4 @@ def stop_server():
 
 if __name__ == '__main__':
     print("starting to listen on control port 5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
