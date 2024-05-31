9d51a6988ff95bc5472747da826db88fbb6dc044
JonathanArns <jonathan.arns@googlemail.com>
2024-05-31 17:48:02 +0200

    accumulate total stats for tpcc

--- bench_harness.py
+++ bench_harness.py
@@ -250,14 +250,23 @@ def run_bench(bench_config, nodes, silent=False):
                 data["cluster_size"] = len(bench_config["cluster_config"]["node_ids"])
                 data["duration"] = bench_config["settings"]["duration"]
                 data["num_clients"] = bench_config["settings"]["num_clients"]
+                data["total_throughput"] = 0.0
+                data["total_mean_latency"] = 0.0
+                data["total_count"] = 0
+                data["total_time"] = 0.0
                 for result in results:
                     try:
                         node_id = result["node_id"]
                         replica_data = json.loads(result['std_out'])
+                        data["total_throughput"] += result["total_throughput"]
+                        data["total_mean_latency"] += result["total_mean_latency"]
+                        data["total_count"] += result["total_count"]
+                        data["total_time"] += result["total_time"]
                         for key, val in replica_data.items():
                             data[f"{node_id}_{key}"] = val
                     except Exception as e:
                         raise Exception(f"couldn't load TPCC results with exception {e}\nSTDOUT:\n{result['std_out']}\nSTDERR:\n{result['std_err']}")
+                data["total_mean_latency"] /= len(results)
                 return data
 
             else:
