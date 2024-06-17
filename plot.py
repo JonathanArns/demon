e73ae06a8d889a09dcf0b2840766bde7233c98dc
JonathanArns <jonathan.arns@googlemail.com>
2024-06-17 14:08:59 +0200

    add explicit plot styles

--- plot.py
+++ plot.py
@@ -4,6 +4,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+PLOT_STYLES = {
+    "demon": "o-",
+    "gemini": "o-",
+    "redblue": "o-",
+    "unistore": "o-",
+    "causal": "o-",
+    "strict": "o-",
+}
+
+COLORS = {
+    "demon": "tab:blue",
+    "gemini": "tab:red",
+    "redblue": "tab:green",
+    "unistore": "tab:orange",
+    "causal": "tab:purple",
+    "strict": "tab:brown",
+}
+
+LABELS = {
+    "demon": "semi-ser",
+    "gemini": "gemini",
+    "redblue": "redblue (new)",
+    "unistore": "unistore",
+    "causal": "causal",
+    "strict": "strict",
+}
+
+
 def plot_tpcc(df, dir_path):
     for cluster_size in df["cluster_size"].unique():
         cluster_df = df[df["cluster_size"] == cluster_size]
@@ -11,7 +39,7 @@ def plot_tpcc(df, dir_path):
         plt.figure(figsize=(10, 6))  # Adjust size as needed
         for proto in protocols:
             proto_df = cluster_df[cluster_df["proto"] == proto]
-            plt.plot(proto_df["total_throughput"], proto_df["total_mean_latency"], marker="o", label=proto)
+            plt.plot(proto_df["total_throughput"], (proto_df["total_time"] * 1000) / proto_df["total_count"], PLOT_STYLES[proto], label=LABELS[proto], color=COLORS[proto])
         plt.xlabel("throughput (txns/s)")
         plt.ylabel("mean latency (ms)")
         plt.title(f"TPCC latency by throughput ({cluster_size} replicas)")
@@ -28,8 +56,10 @@ def plot_micro(df, dir_path):
 def plot_single_micro(df, datatype, cluster_size, dir_path):
     protocols = df["proto"].unique()
     # aggregate the data
-    mean_latencies = {}
     throughputs = {}
+    mean_latencies = {}
+    p95_latencies = {}
+    p99_latencies = {}
     for proto in protocols:
         proto_df = df[df["proto"] == proto]
         grouped = proto_df.groupby("strong_ratio")
@@ -44,25 +74,49 @@ def plot_single_micro(df, datatype, cluster_size, dir_path):
             "x": rows_with_max_throughput["strong_ratio"],
             "y": rows_with_max_throughput["total_mean_latency"],
         }
+        p95_latencies[proto] = {
+            "x": rows_with_max_throughput["strong_ratio"],
+            "y": rows_with_max_throughput["total_p95_latency"],
+        }
+        p99_latencies[proto] = {
+            "x": rows_with_max_throughput["strong_ratio"],
+            "y": rows_with_max_throughput["total_p99_latency"],
+        }
 
-    plt.figure(figsize=(10, 6))  # Adjust size as needed
+    plt.figure(figsize=(10, 10))  # Adjust size as needed
     plt.suptitle(f"{datatype} with {cluster_size} replicas")
 
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 2, 1)
     for proto, vals in mean_latencies.items():
-        plt.plot(vals["x"], vals["y"], marker="o", label=proto)
+        plt.plot(vals["x"], vals["y"], PLOT_STYLES[proto], label=LABELS[proto], color=COLORS[proto])
     plt.xlabel("strong operation ratio")
     plt.ylabel("mean latency (ms)")
-    plt.title(f"{datatype} mean latency at max throughput")
+    plt.title(f"mean latency at max throughput")
     plt.legend()
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 2, 2)
     for proto, vals in throughputs.items():
-        plt.plot(vals["x"], vals["y"], marker="o", label=proto)
+        plt.plot(vals["x"], vals["y"], PLOT_STYLES[proto], label=LABELS[proto], color=COLORS[proto])
     plt.yscale("log")
     plt.xlabel("strong operation ratio")
     plt.ylabel("throughput (ops/s)")
-    plt.title(f"{datatype} max throughput")
+    plt.title(f"max throughput")
+    plt.legend()
+
+    plt.subplot(2, 2, 3)
+    for proto, vals in p95_latencies.items():
+        plt.plot(vals["x"], vals["y"], PLOT_STYLES[proto], label=LABELS[proto], color=COLORS[proto])
+    plt.xlabel("strong operation ratio")
+    plt.ylabel("95th percentile latency (ms)")
+    plt.title(f"95th percentile latency at max throughput")
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    for proto, vals in p99_latencies.items():
+        plt.plot(vals["x"], vals["y"], PLOT_STYLES[proto], label=LABELS[proto], color=COLORS[proto])
+    plt.xlabel("strong operation ratio")
+    plt.ylabel("99th percentile latency (ms)")
+    plt.title(f"99th percentile latency at max throughput")
     plt.legend()
 
     plt.savefig(os.path.join(dir_path, f"{datatype}_plot_{cluster_size}_nodes.png"), dpi=300)
