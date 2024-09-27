71d19ec07b5132063078375cd9822c2a651abeef
JonathanArns <jonathan.arns@googlemail.com>
2024-09-27 19:44:15 +0200

    sort protocols in figures

--- figs.py
+++ figs.py
@@ -35,61 +35,7 @@ LABELS = {
     "strict": "Strict",
 }
 
-
-def plot_rubis(df, dir_path):
-    df = df[df["datatype"] == "rubis"]
-    if len(df) == 0:
-        return
-
-    for cluster_size in df["cluster_size"].unique():
-        cluster_df = df[df["cluster_size"] == cluster_size]
-        protocols = df["proto"].unique()
-        operations = ["GetAuction", "GetItem", "OpenAuction", "CloseAuction", "Bid", "Sell", "BuyNow"] 
-        stats = {proto: [] for proto in protocols}
-        for proto in protocols:
-            proto_df = cluster_df[cluster_df["proto"] == proto]
-            for operation in operations:
-                op_df = proto_df[proto_df["op"] == operation]
-                stats[proto].append(calc_stats(op_df["latency_micros"]))
-
-        plt.figure(figsize=(6, 6))  # Adjust size as needed
-        plt.suptitle(f"Rubis-like benchmark with {cluster_size} replicas")
-        
-        index = np.arange(len(operations))
-        bar_width = 0.1
-
-        i = 0
-        for proto, stats in stats.items():
-            mean = [item["mean"] for item in stats]
-            mean_conf = [item["mean_conf_int"] for item in stats]
-            # p99 = [item["p99"] for item in stats]
-            plt.bar(
-                x=index + 1.5*i*bar_width,
-                height=[item["mean"] for item in stats],
-                yerr=([item["mean"] - item["mean_conf_int"].low for item in stats], [item["mean_conf_int"].high - item["mean"] for item in stats]),
-                width=bar_width,
-                label=proto,
-                capsize=1.0
-            )
-            i += 1
-        plt.xticks(index + 0.5 * 1.5 * bar_width * (len(protocols)-1), operations)
-        plt.ylabel("mean latency (ms)")
-        # plt.title(f"mean latency per operation")
-        plt.legend()
-        plt.savefig(os.path.join(dir_path, f"rubis_bar_plot_{cluster_size}_nodes.png"), dpi=300)
-
-        # plt.subplot(2, 1, 2)
-        # i = 0
-        # for operation, vals in p99_latencies.items():
-        #     plt.bar(index + 1.5*i*bar_width, vals, bar_width, label=operation)
-        #     i += 1
-        # plt.xticks(index + 0.5 * 1.5 * bar_width * (len(protocols)-1), operations)
-        # plt.ylabel("p99 latency (ms)")
-        # # plt.title(f"tail latency per operation")
-        # plt.legend()
-
-        plt.savefig(os.path.join(dir_path, f"rubis_bar_plot_{cluster_size}_nodes.png"), dpi=300)
-    
+PROTOS = ["demon", "gemini", "redblue", "unistore", "strict", "causal"]
 
 
 def aggregate(benches, dir_path, recompute=True):
@@ -147,55 +93,36 @@ def plot_non_neg_counter_op_latency(df, dir_path):
     df = df[df["cluster_size"] == 5]
     df = df[df["num_clients"] == 1000]
     df = df[df["strong_ratio"] == 0.5]
-    protocols = df["proto"].unique()
+    protocols = [proto for proto in PROTOS if proto in df["proto"].unique()]
     ops = ["Add", "Subtract"]
     index = np.arange(len(ops))
     bar_width = 0.15
-    plt.figure(figsize=(6, 6))  # Adjust size as needed
-    i = 0
-    j = 0
-    for proto in protocols:
-        proto_df = df[df["proto"] == proto]
-        plt.bar(
-            x=index + i * bar_width,
-            height=[proto_df[f"{op}_client_mean_latency"].iloc[0] for op in ops],
-            yerr=([0 for x in range(len(ops))], [proto_df[f"{op}_client_p95_latency"].iloc[0] for op in ops]),
-            width=bar_width,
-            label=LABELS[proto],
-            capsize=3.0,
-            color=COLORS[proto],
-        )
-        i += 1
-    plt.xticks([x + 0.5 * bar_width * (len(protocols)-1) for x in range(len(ops))], ops)
-    plt.ylabel("mean client latency (ms)")
-    plt.legend()
-    plt.savefig(os.path.join(dir_path, f"non-neg-counter-client-latency-bars.png"), dpi=300)
-
-    plt.figure(figsize=(6, 6))  # Adjust size as needed
-    i = 0
-    j = 0
-    for proto in protocols:
-        proto_df = df[df["proto"] == proto]
-        plt.bar(
-            x=index + i * bar_width,
-            height=[proto_df[f"{op}_remote_mean_latency"].iloc[0] for op in ops],
-            yerr=([0 for x in range(len(ops))], [proto_df[f"{op}_remote_p95_latency"].iloc[0] for op in ops]),
-            width=bar_width,
-            label=LABELS[proto],
-            capsize=3.0,
-            color=COLORS[proto],
-        )
-        i += 1
-    plt.xticks([x + 0.5 * bar_width * (len(protocols)-1) for x in range(len(ops))], ops)
-    plt.ylabel("mean remote latency (ms)")
-    plt.legend()
-    plt.savefig(os.path.join(dir_path, f"non-neg-counter-remote-latency-bars.png"), dpi=300)
+    for kind in ["remote", "client"]:
+        plt.figure(figsize=(6, 6))  # Adjust size as needed
+        i = 0
+        j = 0
+        for proto in protocols:
+            proto_df = df[df["proto"] == proto]
+            plt.bar(
+                x=index + i * bar_width,
+                height=[proto_df[f"{op}_{kind}_mean_latency"].iloc[0] for op in ops],
+                yerr=([0 for x in range(len(ops))], [proto_df[f"{op}_{kind}_p95_latency"].iloc[0] for op in ops]),
+                width=bar_width,
+                label=LABELS[proto],
+                capsize=3.0,
+                color=COLORS[proto],
+            )
+            i += 1
+        plt.xticks([x + 0.5 * bar_width * (len(protocols)-1) for x in range(len(ops))], ops)
+        plt.ylabel(f"mean {kind} latency (ms)")
+        plt.legend()
+        plt.savefig(os.path.join(dir_path, f"non-neg-counter-{kind}-latency-bars.png"), dpi=300)
 
 def plot_rubis_throughput(df, dir_path):
     df = df[df["datatype"] == "rubis"]
     df = df[df["duration"] == 60]
     df = df[df["cluster_size"] == 5]
-    protocols = df["proto"].unique()
+    protocols = [proto for proto in PROTOS if proto in df["proto"].unique()]
     plt.figure(figsize=(10, 6))  # Adjust size as needed
     for proto in protocols:
         proto_df = df[df["proto"] == proto]
