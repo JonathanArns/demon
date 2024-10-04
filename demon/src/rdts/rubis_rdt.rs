0fda513cd02abdc1225c79eae7e5b3b71880d2b5
JonathanArns <jonathan.arns@googlemail.com>
2024-10-04 16:46:46 +0200

    add auction_id to tracing info for RUBiS

--- demon/src/rdts/rubis_rdt.rs
+++ demon/src/rdts/rubis_rdt.rs
@@ -205,15 +205,15 @@ impl Operation for RubisOp {
 
     fn name(&self) -> String {
         match *self {
-            Self::RegisterUser {..} => "RegisterUser",
-            Self::GetAuction => "GetAuction",
-            Self::OpenAuction {..} => "OpenAuction",
-            Self::CloseAuction {..} => "CloseAuction",
-            Self::Bid {..} => "Bid",
-            Self::GetItem => "GetItem",
-            Self::Sell {..} => "Sell",
-            Self::BuyNow {..} => "BuyNow",
-        }.to_string()
+            Self::RegisterUser { .. } => "RegisterUser".to_string(),
+            Self::GetAuction => "GetAuction".to_string(),
+            Self::OpenAuction {..} => "OpenAuction".to_string(),
+            Self::CloseAuction { auction_id } => format!("CloseAuction {}", auction_id),
+            Self::Bid { auction_id, .. } => format!("Bid {}", auction_id),
+            Self::GetItem => "GetItem".to_string(),
+            Self::Sell {..} => "Sell".to_string(),
+            Self::BuyNow {..} => "BuyNow".to_string(),
+        }
     }
 
     fn apply(&self, state: &mut Self::State) -> Option<Self::ReadVal> {
